7c5af980f2cb9c79c17ade0a7ce746f44ccad98c
Immanuel <emmanuelmendoza448@gmail.com>
2026-03-12 19:16:18 -0600

    panel.Blade

---
+++ resources/views/panel.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Panel Principal - Examen Mascotas</title>
+</head>
+<body>
+    <h2>Panel Administrador</h2>
+
+    <p><a href="/usuarios">Módulo de Usuarios</a></p>
+    <p><a href="/mascotas">Módulo de Mascotas</a></p>
+    <p><a href="/logout">Cerrar sesión</a></p>
+</body>
+</html>
