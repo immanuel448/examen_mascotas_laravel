d6827548f8a52bf25794582aaa90754d7a4f1b3f
Immanuel <emmanuelmendoza448@gmail.com>
2026-03-12 19:16:04 -0600

    login.Blade

---
+++ resources/views/login.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Login - Examen Mascotas</title>
+</head>
+<body>
+    <h2>Login Administrador</h2>
+
+    @if(session('error'))
+        <p style="color:red">{{ session('error') }}</p>
+    @endif
+
+    <form method="POST" action="/login">
+        <!-- CSRF si Laravel lo requiere -->
+        <input type="password" name="password" placeholder="Contraseña" required>
+        <button type="submit">Ingresar</button>
+    </form>
+</body>
+</html>
